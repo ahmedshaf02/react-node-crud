e3053dd108d96b1a9e85fd9aece4e613dad4bad1
Shafiq Ahmed <ahmedshaf02@gmail.com>
2020-06-26 23:19:31 +0530

    react-employee-crud-project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+import EmployeeDetails from "./EmployeeDetails"
+import EmployeeForm from "./EmployeeForm"
+
+
+export default function App() {
+  return (
+    <div className="App">
+     <EmployeeForm/>
+     {/* <EmployeeDetails/> */}
+    </div>
+  );
+}
